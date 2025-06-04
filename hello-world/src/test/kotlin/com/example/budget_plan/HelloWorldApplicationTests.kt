abc6acaf5bc0bba1d7fd3df7715e012bf2ebeff3
annasazonovaa <sazonovaanna654@gmail.com>
2025-06-04 22:32:39 +0600

    5

--- hello-world/src/test/kotlin/com/example/budget_plan/HelloWorldApplicationTests.kt
+++ hello-world/src/test/kotlin/com/example/budget_plan/HelloWorldApplicationTests.kt
@@ -3,7 +3,7 @@ package com.example.budget_plan
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [BudgetPlanApplication::class])
 class HelloWorldApplicationTests {
 
 	@Test
